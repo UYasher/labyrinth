cbd75049a710d117ab423d88cf6596518ccd2562
UYasher <shriyash.upadhyay@gmail.com>
2021-12-31 03:15:47 -0500

    [FEAT] Reorganized code into sections

--- src/GameState.hs
+++ src/GameState.hs
@@ -9,7 +9,6 @@ import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
 import Tile
 
--- The board + extraTile must have as many starts as there are players
 data GameState = Game
   { board :: Board, -- The tiles currently in play
     numPlayers :: Int, -- The number of players, $n$
@@ -22,6 +21,7 @@ data GameState = Game
   deriving (Show)
 
 -- Test this
+-- The board + extraTile must have as many starts as there are players, otherwise this function returns Nothing
 initializePlayerLocations :: GameState -> Maybe GameState
 initializePlayerLocations g@Game {numPlayers = n} = updateGameState <$> initialPlayerTiles (board g) n
   where
@@ -60,6 +60,8 @@ acGame2 =
         insertCols = defaultInsertCols
       }
 
+{- Tile Insertion -}
+
 -- Take in a board tile, row number, and boolean (representing front/back)
 -- Returns the tile which was pushed off the board, as well as the board with the tile inserted into the front/back of the row
 insertTileRow :: Board -> Tile -> Int -> Bool -> (Tile, Board)
@@ -85,40 +87,6 @@ insertTileCol b t r front = transposeBoard <$> insertTileRow (transposeBoard b) 
 
 insertTile :: Board -> Tile -> Bool -> Int -> Bool -> (Tile, Board)
 insertTile b t isRow = (if isRow then insertTileRow else insertTileCol) b t
-
-inBounds :: Board -> Location -> Bool
-inBounds b (x, y) = 0 <= x && x < height b && 0 <= y && y < width b
-
-(+.) :: Location -> Location -> Location
-(+.) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
-
--- TODO: Make this function more elegant, it's currently a bit difficult to understand
-adjacent :: Board -> Location -> [Location]
-adjacent board loc = map snd $ filter (uncurry (connected board loc)) $ filter (inBounds board . snd) $ zip adjacentLocations $ map (+. loc) adjacentLocations
-  where
-    adjacentLocations = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    connected b l d l'
-      | d == (0, 1) = Tile.Right `elem` shape (getLocation l b) && Tile.Left `elem` shape (getLocation l' b)
-      | d == (0, -1) = Tile.Left `elem` shape (getLocation l b) && Tile.Right `elem` shape (getLocation l' b)
-      | d == (1, 0) = Tile.Down `elem` shape (getLocation l b) && Tile.Up `elem` shape (getLocation l' b)
-      | d == (-1, 0) = Tile.Up `elem` shape (getLocation l b) && Tile.Down `elem` shape (getLocation l' b)
-      | otherwise = error $ "impossible direction: " ++ show d
-
---     Board -> Current  -> Visited          -> Reachable
-dfs :: Board -> Location -> Set.Set Location -> Set.Set Location
-dfs _ loc visited | loc `elem` visited = visited
-dfs b loc visited = foldr (dfs b) (Set.insert loc visited) (adjacent b loc)
-
--- Give the state of the game, find all tiles the player can move to
-findSCC :: Board -> Location -> Set.Set Int
-findSCC b loc = Set.map toTileNumbers reachable
-  where
-    reachable = dfs b loc Set.empty
-    toTileNumbers = number . flip getLocation b
-
--- Helper function to convert a board into a subset of the tiles
-boardToSubset :: Board -> Set.Set Int -> Board
-boardToSubset (Board b) s = Board $ map (map (\tile -> if number tile `elem` s then tile else fromShape Set.empty)) b
 
 double :: [a] -> [a]
 double [] = []
@@ -154,6 +122,42 @@ applyInsertMoveOrientation g@Game {extraTile = t} (o, a, b, c) = applyInsertMove
 
 allInsertions :: GameState -> [GameState]
 allInsertions g@Game {extraTile = t} = map (applyInsertMoveGame g) $ allInsertMoves t (insertRows g) (insertCols g)
+
+{- Finding Strongly Connected Components -}
+
+inBounds :: Board -> Location -> Bool
+inBounds b (x, y) = 0 <= x && x < height b && 0 <= y && y < width b
+
+(+.) :: Location -> Location -> Location
+(+.) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- TODO: Make this function more elegant, it's currently a bit difficult to understand
+adjacent :: Board -> Location -> [Location]
+adjacent board loc = map snd $ filter (uncurry (connected board loc)) $ filter (inBounds board . snd) $ zip adjacentLocations $ map (+. loc) adjacentLocations
+  where
+    adjacentLocations = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    connected b l d l'
+      | d == (0, 1) = Tile.Right `elem` shape (getLocation l b) && Tile.Left `elem` shape (getLocation l' b)
+      | d == (0, -1) = Tile.Left `elem` shape (getLocation l b) && Tile.Right `elem` shape (getLocation l' b)
+      | d == (1, 0) = Tile.Down `elem` shape (getLocation l b) && Tile.Up `elem` shape (getLocation l' b)
+      | d == (-1, 0) = Tile.Up `elem` shape (getLocation l b) && Tile.Down `elem` shape (getLocation l' b)
+      | otherwise = error $ "impossible direction: " ++ show d
+
+--     Board -> Current  -> Visited          -> Reachable
+dfs :: Board -> Location -> Set.Set Location -> Set.Set Location
+dfs _ loc visited | loc `elem` visited = visited
+dfs b loc visited = foldr (dfs b) (Set.insert loc visited) (adjacent b loc)
+
+-- Give the state of the game, find all tiles the player can move to
+findSCC :: Board -> Location -> Set.Set Int
+findSCC b loc = Set.map toTileNumbers reachable
+  where
+    reachable = dfs b loc Set.empty
+    toTileNumbers = number . flip getLocation b
+
+-- Helper function to convert a board into a subset of the tiles
+boardToSubset :: Board -> Set.Set Int -> Board
+boardToSubset (Board b) s = Board $ map (map (\tile -> if number tile `elem` s then tile else fromShape Set.empty)) b
 
 -- Given the game state, the player location, and the tile they want to reach, return all methods of reaching that tile
 -- The methods are tuples of the form (isRow, row/col, isFront, moveLocation)
