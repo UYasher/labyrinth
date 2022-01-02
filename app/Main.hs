a7d7de51d1f1ccbda202f75866fc4646820b7d99
UYasher <shriyash.upadhyay@gmail.com>
2022-01-01 22:05:38 -0500

    [FEAT] Print title text on start

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,23 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import Lib
+import Lib (game, program)
+import Text.RawString.QQ (r)
+
+titleText :: String
+titleText =
+  [r|
+ __          ___      .______   ____    ____ .______       __  .__   __. .___________. __    __  
+|  |        /   \     |   _  \  \   \  /   / |   _  \     |  | |  \ |  | |           ||  |  |  | 
+|  |       /  ^  \    |  |_)  |  \   \/   /  |  |_)  |    |  | |   \|  | `---|  |----`|  |__|  | 
+|  |      /  /_\  \   |   _  <    \_    _/   |      /     |  | |  . `  |     |  |     |   __   | 
+|  `----./  _____  \  |  |_)  |     |  |     |  |\  \----.|  | |  |\   |     |  |     |  |  |  | 
+|_______/__/     \__\ |______/      |__|     | _| `._____||__| |__| \__|     |__|     |__|  |__| 
+|]
 
 main :: IO ()
 main = do
-    program game
-    pure ()
+  putStrLn titleText
+  program game
+  pure ()
